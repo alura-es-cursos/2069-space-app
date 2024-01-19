d888255186126233526644c58a8f7db29faed34a
Daniel <dannyriverol@hotmail.com>
2024-01-19 13:43:58 -0300

    Estado inicial del Proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import styled from "styled-components"
-import GlobalStyles from "./components/GlobalStyles"
-const FondoGradiente =  styled.div`
-background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
-width:100%;
-min-height:100vh;
-`
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  
+  const [count, setCount] = useState(0)
 
   return (
     <>
-    <FondoGradiente>
-    <GlobalStyles/>
-    </FondoGradiente>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
