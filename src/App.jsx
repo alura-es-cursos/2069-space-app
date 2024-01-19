790046ac57c1bc486e7d501ed7d0d2661fa4b8b7
Daniel <dannyriverol@hotmail.com>
2024-01-19 13:39:30 -0300

    Estado inicial del Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+import GlobalStyles from "./components/GlobalStyles"
+const FondoGradiente =  styled.div`
+background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+width:100%;
+min-height:100vh;
+`
+
+function App() {
+  
+
+  return (
+    <>
+    <FondoGradiente>
+    <GlobalStyles/>
+    </FondoGradiente>
+    </>
+  )
+}
+
+export default App
